587e3f9a067641138ea66b0e8af939be839836fa
Al Stone <astone@heckgrammar.co.uk>
2023-11-27 11:19:51 +0000

    Add project files.

---
+++ SectionB2212C/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SectionB2212C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Enter a string: ");
+            input = Console.ReadLine();
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            Stack<char> vowelOrder = new Stack<char>();
+            for(int i = 0;i<input.Length;i++)
+            {
+                if (vowels.Contains(input[i]))
+                {
+                    vowelOrder.Push(input[i]);
+                }
+            }
+            string output = "";
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (vowels.Contains(input[i]))
+                {
+                    output += vowelOrder.Pop();
+                }
+                else
+                {
+                    output += input[i];
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
